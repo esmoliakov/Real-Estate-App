34ba6ab75674507b4a82d90450c5f3b12e580eee
esmoliakov <e.smoliakovas@gmail.com>
2025-11-03 16:59:58 +0200

    fixed compile errors

--- RealEstateApp/Shared/Models/User.cs
+++ RealEstateApp/Shared/Models/User.cs
@@ -3,6 +3,6 @@ namespace RealEstateApp.Shared.Models;
 public class User
 {
     public int Id { get; set; }
-    public string Username { get; set; }
-    public string PasswordHash { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
 }
